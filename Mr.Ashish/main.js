6d1f3c60f57df773d04c9b995b7eec6933adfd0c
Ashish Sharma <94701564+Ashish813213@users.noreply.github.com>
2022-02-03 20:42:36 +0530

    2.0 js

--- Mr.Ashish/main.js
+++ Mr.Ashish/main.js
@@ -1,78 +1,125 @@
 let tag = document.getElementById('tag');
-        let nav = document.getElementById('navUL');
-        // tag.addEventListener('click',()=>{
-        //     if(nav.style.height == '0px'){
-        //         nav.style.height = "calc(100vh - 70px)";
-        //     }
-        //     else{
-        //         nav.style.height = "0px";
-        //     }
-        // })
-        tag.addEventListener('click',()=>{
-            if (nav.classList.contains('closing')) {
-                nav.classList.replace('closing','navOPEN');
-            }
-            else{
-                nav.classList.add("closing")
-                nav.classList.remove('navOPEN')
-            }
-        })
-        let logBtn = document.getElementById('logbtn')
-        let log = document.getElementById('logContent')
-        let notN = document.getElementById('not')
-        logBtn.addEventListener('click', () => {
-            if (log.style.width == "0%") {
-                log.style.width = "100%" ;
-                log.style.opacity = "1";
-            }
-            else {
-                log.style.width = "0%"
-                log.style.opacity = "0"
-            }
-        })
-        notN.addEventListener('click', () => {
-            if (log.style.width == "0%") {
-                log.style.width = "100%" ;
-                log.style.opacity = "1";
-            }
-            else {
-                log.style.width = "0%"
-                log.style.opacity = "0"
-            }
-        })
-        window.addEventListener('scroll',(e)=>{
-            let scrollVAlue = pageYOffset;
-            let card1 = document.getElementById('card1')
-            let card2 = document.getElementById('card2')
-            let card3 = document.getElementById('card3')
-            let bar1 = document.getElementById('bar1');
-            let bar2 = document.getElementById('bar2');
-            let bar3 = document.getElementById('bar3');
-            let bar4 = document.getElementById('bar4');
-            let bar5 = document.getElementById('bar5');
+let nav = document.getElementById('navUL');
 
-            if(scrollVAlue => 180){
-                card1.classList.add('animate1');
-                card2.classList.add('animate1');
-                card3.classList.add('animate1');
-            }
-            else{
-                card1.classList.remove('animate1');
-                card2.classList.remove('animate1');
-                card3.classList.remove('animate1');
-            }
-            if (scrollVAlue => 550 ) {
-                bar1.classList.add('animate2');
-                bar2.classList.add('animate2');
-                bar3.classList.add('animate2');
-                bar4.classList.add('animate2');
-                bar5.classList.add('animate2');
-            }
-            else{
-                bar1.classList.remove('animate2');
-                bar2.classList.remove('animate2');
-                bar3.classList.remove('animate2');
-                bar4.classList.remove('animate2');
-                bar5.classList.remove('animate2');
-            }
-        })
+tag.addEventListener('click', () => {
+    if (nav.classList.contains('closing')) {
+        nav.classList.replace('closing', 'navOPEN');
+    }
+    else {
+        nav.classList.add("closing")
+        nav.classList.remove('navOPEN')
+    }
+})
+let logBtn = document.getElementById('logbtn')
+let log = document.getElementById('logContent')
+let notN = document.getElementById('not')
+logBtn.addEventListener('click', () => {
+    if (log.style.width == "0%") {
+        log.style.width = "100%";
+        log.style.opacity = "1";
+    }
+    else {
+        log.style.width = "0%"
+        log.style.opacity = "0"
+    }
+})
+notN.addEventListener('click', () => {
+    if (log.style.width == "0%") {
+        log.style.width = "100%";
+        log.style.opacity = "1";
+    }
+    else {
+        log.style.width = "0%"
+        log.style.opacity = "0"
+    }
+})
+let showBTN = document.getElementById('hide');
+showBTN.addEventListener('click', ()=>{
+    if(passB.getAttribute('type') == 'password'){
+    passB.setAttribute("type", "text")
+    }
+    else{
+        passB.setAttribute("type", "password")
+    }
+})
+let SubBTN = document.getElementById('login');
+let inputB = document.getElementById('email');
+let passB = document.getElementById('pass');
+SubBTN.addEventListener('click',(e)=>{
+    if(inputB.value == "Ashishsharma@gmail.com" && passB.value == "DEVrocks"){
+        console.log("chal raha hai ");
+        logBtn.innerHTML = "HI,ASHISH";
+        logBtn.click()
+        trueD()
+        clear()
+    }
+    else{
+        console.log("error"); 
+        e.preventDefault();
+        errorDi()
+    }
+})
+let trueD = ()=>{
+    let body = document.querySelector('body')
+    let errorD =  document.createElement('div');
+    let errorT = document.createElement('p');
+    errorT.innerText = "welcome !! Mr.Ashish" ;
+    errorD.classList.add('LogDia');
+    errorD.classList.add('loggedIN');
+    errorD.appendChild(errorT);
+    body.append(errorD);
+    let remover = ()=>{
+           body.removeChild(errorD);
+       }
+    setTimeout(remover,2000);
+
+}
+let errorDi = ()=>{
+    let body = document.querySelector('body')
+    let errorD =  document.createElement('div');
+    let errorT = document.createElement('p');
+    errorT.innerText = "oops error !!!";
+    errorD.classList.add('LogDia');
+    errorD.classList.add('ERRORIN');
+    errorD.appendChild(errorT);
+    body.append(errorD);
+    let remover = ()=>{
+           body.removeChild(errorD);
+       }
+    setTimeout(remover,2000);
+}
+let clear = ()=>{
+    inputB.value = " ";
+    passB.value = " ";
+}
+window.addEventListener('scroll',()=>{
+    let cardC = document.querySelector('.projectC');
+    let cardP = cardC.getBoundingClientRect().top;
+    let screenP = window.innerHeight;
+    let cards = document.querySelectorAll('.project')
+    for (let i = 0; i < cards.length; i++) {
+        if (cardP < screenP) {
+            cards[i].classList.add('active')
+        }
+        else{
+            cards[i].classList.remove('active')
+        }
+    } 
+})
+
+window.addEventListener('scroll' , ()=>{
+    let bar = document.querySelector('.skillc');
+    let barP = bar.getBoundingClientRect().top;
+    console.log(barP);
+    let screenP = window.innerHeight;
+    let bars = document.querySelectorAll('.barS');
+    for (let i = 0; i < bars.length; i++) {
+        if (barP < screenP)  {
+            bars[i].classList.add('animate2')
+        }
+        else{
+            bars[i].classList.remove('animate2')
+        }
+        
+    }
+})
